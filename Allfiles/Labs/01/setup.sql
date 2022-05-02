40c903f5fdc8ada117ad48b9d95e906c40aebd37
James Herring <49248205+TheJamesHerring@users.noreply.github.com>
2022-05-02 12:09:42 -0400

    Update setup.sql
    Removed files that were un-needed

--- Allfiles/Labs/01/setup.sql
+++ Allfiles/Labs/01/setup.sql
@@ -2,33 +2,7 @@
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[FactInternetSales](
-	[SalesOrderNumber] [nvarchar](20) NOT NULL,
-	[SalesOrderLineNumber] [tinyint] NOT NULL,
-	[CustomerKey] [int] NOT NULL,
-	[ProductKey] [int] NOT NULL,
-	[OrderDateKey] [int] NOT NULL,
-	[DueDateKey] [int] NOT NULL,
-	[ShipDateKey] [int] NULL,
-	[PromotionKey] [int] NOT NULL,
-	[CurrencyKey] [int] NOT NULL,
-	[SalesTerritoryKey] [int] NOT NULL,
-	[OrderQuantity] [smallint] NOT NULL,
-	[UnitPrice] [money] NOT NULL,
-	[ExtendedAmount] [money] NOT NULL,
-	[UnitPriceDiscountPct] [decimal](7, 4) NOT NULL,
-	[DiscountAmount] [float] NOT NULL,
-	[ProductStandardCost] [money] NOT NULL,
-	[TotalProductCost] [money] NOT NULL,
-	[SalesAmount] [money] NOT NULL,
-	[TaxAmount] [money] NOT NULL,
-	[FreightAmount] [money] NOT NULL,
-	[CarrierTrackingNumber] [nvarchar](25) NULL,
-	[CustomerPONumber] [nvarchar](25) NULL,
-	[RevisionNumber] [tinyint] NOT NULL
-)
 
-GO
 CREATE TABLE [dbo].[DimCustomer](
 	[CustomerKey] [int] IDENTITY(1,1) NOT NULL,
 	[GeographyKey] [int] NULL,
@@ -61,44 +35,7 @@ CREATE TABLE [dbo].[DimCustomer](
 	[CommuteDistance] [nvarchar](15) NULL
 )
 
-GO
-CREATE TABLE [dbo].[DimDate](
-	[DateKey] [int] NOT NULL,
-	[FullDateAlternateKey] [date] NOT NULL,
-	[DayNumberOfWeek] [tinyint] NOT NULL,
-	[EnglishDayNameOfWeek] [nvarchar](10) NOT NULL,
-	[SpanishDayNameOfWeek] [nvarchar](10) NOT NULL,
-	[FrenchDayNameOfWeek] [nvarchar](10) NOT NULL,
-	[DayNumberOfMonth] [tinyint] NOT NULL,
-	[DayNumberOfYear] [smallint] NOT NULL,
-	[WeekNumberOfYear] [tinyint] NOT NULL,
-	[EnglishMonthName] [nvarchar](10) NOT NULL,
-	[SpanishMonthName] [nvarchar](10) NOT NULL,
-	[FrenchMonthName] [nvarchar](10) NOT NULL,
-	[MonthNumberOfYear] [tinyint] NOT NULL,
-	[CalendarQuarter] [tinyint] NOT NULL,
-	[CalendarYear] [smallint] NOT NULL,
-	[CalendarSemester] [tinyint] NOT NULL,
-	[FiscalQuarter] [tinyint] NOT NULL,
-	[FiscalYear] [smallint] NOT NULL,
-	[FiscalSemester] [tinyint] NOT NULL
-)
 
-GO
-CREATE TABLE [dbo].[DimGeography](
-	[GeographyKey] [int] IDENTITY(1,1) NOT NULL,
-	[City] [nvarchar](30) NULL,
-	[StateProvinceCode] [nvarchar](3) NULL,
-	[StateProvinceName] [nvarchar](50) NULL,
-	[CountryRegionCode] [nvarchar](3) NULL,
-	[EnglishCountryRegionName] [nvarchar](50) NULL,
-	[SpanishCountryRegionName] [nvarchar](50) NULL,
-	[FrenchCountryRegionName] [nvarchar](50) NULL,
-	[PostalCode] [nvarchar](15) NULL,
-	[SalesTerritoryKey] [int] NULL,
-	[IpAddressLocator] [nvarchar](15) NULL)
-
-GO
 CREATE TABLE [dbo].[DimProduct](
 	[ProductKey] [int] IDENTITY(1,1) NOT NULL,
 	[ProductAlternateKey] [nvarchar](25) NULL,
@@ -140,61 +77,4 @@ WITH
   (   
     CLUSTERED INDEX (ProductKey)  
   ); 
-GO
-
-CREATE TABLE [dbo].[DimProductCategory](
-	[ProductCategoryKey] [int] IDENTITY(1,1) NOT NULL,
-	[ProductCategoryAlternateKey] [int] NULL,
-	[EnglishProductCategoryName] [nvarchar](50) NOT NULL,
-	[SpanishProductCategoryName] [nvarchar](50) NOT NULL,
-	[FrenchProductCategoryName] [nvarchar](50) NOT NULL)
-
-GO
-CREATE TABLE [dbo].[DimProductSubcategory](
-	[ProductSubcategoryKey] [int] IDENTITY(1,1) NOT NULL,
-	[ProductSubcategoryAlternateKey] [int] NULL,
-	[EnglishProductSubcategoryName] [nvarchar](50) NOT NULL,
-	[SpanishProductSubcategoryName] [nvarchar](50) NOT NULL,
-	[FrenchProductSubcategoryName] [nvarchar](50) NOT NULL,
-	[ProductCategoryKey] [int] NULL)
-GO
-
-CREATE TABLE [dbo].[DimSalesTerritory](
-	[SalesTerritoryKey] [int] IDENTITY(1,1) NOT NULL,
-	[SalesTerritoryAlternateKey] [int] NULL,
-	[SalesTerritoryRegion] [nvarchar](50) NOT NULL,
-	[SalesTerritoryCountry] [nvarchar](50) NOT NULL,
-	[SalesTerritoryGroup] [nvarchar](50) NULL,
-	[SalesTerritoryImage] [varbinary](max) NULL)
-WITH  
-  (   
-    CLUSTERED INDEX (SalesTerritoryKey)  
-  ); 
-GO
-
-CREATE TABLE [dbo].[DimPromotion](
-	[PromotionKey] [int] IDENTITY(1,1) NOT NULL,
-	[PromotionAlternateKey] [int] NULL,
-	[EnglishPromotionName] [nvarchar](255) NULL,
-	[SpanishPromotionName] [nvarchar](255) NULL,
-	[FrenchPromotionName] [nvarchar](255) NULL,
-	[DiscountPct] [float] NULL,
-	[EnglishPromotionType] [nvarchar](50) NULL,
-	[SpanishPromotionType] [nvarchar](50) NULL,
-	[FrenchPromotionType] [nvarchar](50) NULL,
-	[EnglishPromotionCategory] [nvarchar](50) NULL,
-	[SpanishPromotionCategory] [nvarchar](50) NULL,
-	[FrenchPromotionCategory] [nvarchar](50) NULL,
-	[StartDate] [datetime] NOT NULL,
-	[EndDate] [datetime] NULL,
-	[MinQty] [int] NULL,
-	[MaxQty] [int] NULL)
-GO
-
-CREATE TABLE [dbo].[DimCurrency](
-	[CurrencyKey] [int] IDENTITY(1,1) NOT NULL,
-	[CurrencyAlternateKey] [nchar](3) NOT NULL,
-	[CurrencyName] [nvarchar](50) NOT NULL,
-	[FormatString] [nvarchar](20) NULL)
-
 GO
