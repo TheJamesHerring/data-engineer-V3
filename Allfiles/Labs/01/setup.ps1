e7debd7d70c3ebaff12a46325fbafe6704dfbead
James Herring <49248205+TheJamesHerring@users.noreply.github.com>
2022-05-11 18:20:19 -0400

    removed bcp components and added load csv

--- Allfiles/Labs/01/setup.ps1
+++ Allfiles/Labs/01/setup.ps1
@@ -132,4 +132,17 @@ Get-ChildItem "./data/*.txt" -File | Foreach-Object {
 write-host "Pausing the $sqlDatabaseName SQL Pool..."
 Suspend-AzSynapseSqlPool -WorkspaceName $synapseWorkspace -Name $sqlDatabaseName -AsJob
 
+# Upload files to blob container
+write-host "Loading data..."
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
+$storageContext = $storageAccount.Context
+Get-ChildItem "./data/*.csv" -File | Foreach-Object {
+    write-host ""
+    $file = $_.Name
+    Write-Host $file
+    $blobPath = "sales/customers/$file"
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
 write-host "Script completed at $(Get-Date)"
+
