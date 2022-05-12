59d8f298903b030d9620e85651f426b049e11805
James Herring <49248205+TheJamesHerring@users.noreply.github.com>
2022-05-11 21:03:32 -0400

    need another region for ps to get-random

--- Allfiles/Labs/01/setup.ps1
+++ Allfiles/Labs/01/setup.ps1
@@ -47,8 +47,8 @@ $resourceGroupName = "dp000-$suffix"
 Write-Host "Finding an available region. This may take several minutes...";
 $delay = 0, 30, 60, 90, 120 | Get-Random
 Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
-#$preferred_list = "westus"
-$preferred_list = "westus"
+#$preferred_list = "westus","westus2"
+$preferred_list = "westus","westus2"
 $locations = Get-AzLocation | Where-Object {
     $_.Providers -contains "Microsoft.Synapse" -and
     $_.Providers -contains "Microsoft.Sql" -and
